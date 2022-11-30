a100238567786b44fee78c59bfdbdfbd06d88e93
gabriel <gabriell.chaves00@gmail.com>
2022-11-30 19:37:37 -0300

    modificacion

--- Ejercicio_15/src/ejercicio_15/Ejercicio_15.java
+++ Ejercicio_15/src/ejercicio_15/Ejercicio_15.java
@@ -16,7 +16,7 @@ public class Ejercicio_15 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Rellenando vector 100 Primeros números entereos");
+        System.out.println("hola dani");
         int[] numeros = new int[100];
         llenarVector(numeros);
         System.out.println("Mostrando Vector .... ");
