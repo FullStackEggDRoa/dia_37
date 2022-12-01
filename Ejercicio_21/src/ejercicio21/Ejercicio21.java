a22448528fc576fb0e885cdc5a85e3776f799188
Daniel Roa <droa@apgslt.com>
2022-12-01 17:28:11 -0500

    Modificaciones Ejercicio_21

--- Ejercicio_21/src/ejercicio21/Ejercicio21.java
+++ Ejercicio_21/src/ejercicio21/Ejercicio21.java
@@ -20,26 +20,30 @@ public class Ejercicio21{
     static Random rand = new Random();
     
     
-        public static void RellenarMatriz(int [][] M1,int [][] M2){
-            //Este Sub solo rellene las matrices pidiendo que se ingresen los valores por consola
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                System.out.println("Ingrese un Numero:");
-                    M1[i][j] = read.nextInt();
-                    
+        public static void RellenarMatriz(int[][] M1, int[][] M2) {
+        //Este Sub solo rellene las matrices pidiendo que se ingresen los valores por consola
+         System.out.println("Ingresar Valores Matriz No. 1");
+        for (int i = 0; i < M1.length; i++) {
+            for (int j = 0; j < M1.length; j++) {
+               
+                System.out.println("Fila No.[" + (i + 1) + "]");
+                System.out.println("Ingrese un Numero - MatrizPos[" + i + "][" + j + "]:");
+                M1[i][j] = read.nextInt();
+
             }
             System.out.println("Siguente fila");
-            
+
         }
-            System.out.println("Rellenar matriz 2");
-          for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.println("Ingrese un Numero:");
-                    M2[i][j] = read.nextInt();
-                    
+        System.out.println("Ingresar Valores Matriz No. 2");
+        for (int i = 0; i < M2.length; i++) {
+            for (int j = 0; j < M2.length; j++) {
+
+                System.out.println("Ingrese un Numero - MatrizPos[" + i + "][" + j + "]:");
+                M2[i][j] = read.nextInt();
+
             }
-              System.out.println("Siguiente fila");
-            
+            System.out.println("Siguiente fila");
+
         }
     }
     
@@ -50,50 +54,42 @@ public class Ejercicio21{
         int i2 =0,j2=0, i3 =0, j3=0;
         
         
-        
-        for (int i = 0; i < 6; i++) {
+        // Busqueda de primera coincidencia.
+        for (int i = 0; i < M1.length; i++) {
             if (pos1i != -1) {
                 break;
             }
-           for (int j = 0; j < 6; j++) {
+           for (int j = 0; j < M1.length; j++) {
                 if (M1[i][j]== M2[i2][j2]) {
                     
-                    pos1i = i;
+                    pos1i=i;
                     pos1j=j;
-                    
-                    
+                 
                 }
-             
-                
+               
             }
-          
-           
-           
+       
         }
         
-        
-        System.out.println(pos1i+"y"+pos1j);
-        for (int i = pos1i; i < pos1i+3; i++) {
-            
-            
-            for (int j = pos1j; j < pos1j+3; j++) {
-                if (M1[i][j]== M2[i2][j2]){
+        System.out.println("Validadno los demás elementos de acuerdo a esta posición:");
+        System.out.println("Posicion en Matriz 1 ["+pos1i + "]["+ pos1j+"]");
+        for (int i = pos1i; i < pos1i + M2.length; i++) {
+
+            for (int j = pos1j; j < pos1j + M2.length; j++) {
+                if (M1[i][j] == M2[i2][j2]) {
                     PocicionM2i[i3] = i;
-                PocicionM2j[j3] = j;
-               
-                
-                 }
-                 j3=j3+1; 
-                
-                 j2=j2+1;
+                    PocicionM2j[j3] = j;
+                }
+                j3++;
+                j2++;
             }
             
             j2=pos1j;
-            i3=i3+1;
-            i2=i2+1;
+            i3++;
+            i2++;
            
         }
-        for (int i = 0; i < 10-1; i++) {
+        for (int i = 0; i < PocicionM2i.length; i++) {
             System.out.println("i"+PocicionM2i[i]+"j"+PocicionM2j[i]);
         }
              
@@ -103,20 +99,21 @@ public class Ejercicio21{
     public static void MostrarMatriz (int [][]M1,int [][] M2){
         
         //Este Sub solamente mostrara las matrices
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < M1.length; i++) {
+            for (int j = 0; j < M1.length; j++) {
                 System.out.print("["+M1[i][j]+"]");
             }
             System.out.println(" ");
         }
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < M2.length; i++) {
+            for (int j = 0; j < M2.length; j++) {
                 System.out.print("["+M2[i][j]+"]");
             }
             System.out.println(" ");
         }
     }
     public static void main(String[] args) {
+        
         //Pedimos tamaños de matrices
         System.out.println("Ingrese el tamaño de la matriz numero 1:");
         int N1 = read.nextInt();
@@ -133,9 +130,11 @@ public class Ejercicio21{
         int [] PocicionM2j = new int [N2*N2];
         
         //Llamamos a las funciones
-        
+        System.out.println("Rellenando Matrices .... ");
         RellenarMatriz(M1,M2);
+        System.out.println("Mostrando Matrices Matrices ....");
         MostrarMatriz(M1,M2);
+        System.out.println("Buscando Matriz 2 en Matriz 1 .... ");
         EncontrarMatrizDos(M1,M2,PocicionM2i,PocicionM2j);
         
     }
